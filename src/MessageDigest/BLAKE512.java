c9b1c7619e6088573aa87b7bc6cbd5473407885c
marc.greim <marc.greim@localhost>
2011-11-15 16:54:59 +0100

    performance improvement 1 : 	- removed modulo operation  (warning: to increase the round count the array "perm" has to be extended 	- replaced some for-loops

--- src/MessageDigest/BLAKE512.java
+++ src/MessageDigest/BLAKE512.java
@@ -34,7 +34,13 @@ public class BLAKE512 extends java.security.MessageDigest {
         {12,  5 , 1, 15, 14, 13 , 4 ,10 , 0 , 7 , 6 , 3 , 9 , 2 , 8 ,11},
         {13, 11 , 7 ,14, 12,  1 , 3 , 9 , 5 , 0, 15,  4 , 8 , 6 , 2 ,10},
         {6 ,15 ,14  ,9, 11 , 3 , 0 , 8, 12 , 2 ,13 , 7 , 1 , 4, 10 , 5},
-        {10 , 2 , 8 , 4 , 7 , 6 , 1 , 5 ,15, 11 , 9 ,14  ,3, 12, 13 , 0}      
+        {10 , 2 , 8 , 4 , 7 , 6 , 1 , 5 ,15, 11 , 9 ,14  ,3, 12, 13 , 0},
+        {0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15},
+        {14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3},
+        {11,  8 ,12 , 0  ,5 , 2 ,15 ,13, 10 ,14 , 3 , 6 , 7 , 1 , 9 , 4},
+        {7,  9 , 3 , 1 ,13 ,12 ,11 ,14 , 2 , 6 , 5 ,10 , 4 , 0 ,15 , 8},
+        {9 , 0 , 5 , 7 , 2 , 4 ,10, 15 ,14 , 1 ,11 ,12 , 6 , 8 , 3 ,13},
+        {2, 12 , 6 ,10 , 0 ,11 , 8 , 3 , 4, 13 , 7 , 5 ,15, 14,  1 , 9}
     };
     
     private static final long initialvalue[] = {
@@ -141,7 +147,7 @@ public class BLAKE512 extends java.security.MessageDigest {
         int c = Gindex[i][2];
         int d = Gindex[i][3];
         
-        v[a] = v[a] + v[b] + ( m[perm[r%10][2*i]] ^ constant[perm[r%10][(2*i)+1]]);
+        v[a] = v[a] + v[b] + ( m[perm[r][2*i]] ^ constant[perm[r][(2*i)+1]]);
         
         v[d] = java.lang.Long.rotateRight((v[d] ^ v[a]),32);
         
@@ -149,7 +155,7 @@ public class BLAKE512 extends java.security.MessageDigest {
         
         v[b] = java.lang.Long.rotateRight((v[b] ^ v[c]),25);
         
-        v[a] = v[a] + v[b] + ( m[perm[r%10][(2*i)+1]] ^ constant[perm[r%10][2*i]]);
+        v[a] = v[a] + v[b] + ( m[perm[r][(2*i)+1]] ^ constant[perm[r][2*i]]);
         
         v[d] = java.lang.Long.rotateRight((v[d] ^ v[a]),16);
         
@@ -159,14 +165,24 @@ public class BLAKE512 extends java.security.MessageDigest {
         
     }
     private void Round(int r,long[] v,long[] m){
-        for (int i = 0;i<8;i++){
-            G(i,r,v,m);
-        }
+        G(0,r,v,m);
+        G(1,r,v,m);
+        G(2,r,v,m);
+        G(3,r,v,m);
+        G(4,r,v,m);
+        G(5,r,v,m);
+        G(6,r,v,m);
+        G(7,r,v,m);
     }
     private void Finalize (long[] v,long[] h,long[] s){
-        for (int i = 0;i<8;i++){
-            h[i] = h[i] ^ s[i%4] ^ v[i] ^ v[i+8];
-        }
+        h[0] = h[0] ^ s[0] ^ v[0] ^ v[8];
+        h[1] = h[1] ^ s[1] ^ v[1] ^ v[9];
+        h[2] = h[2] ^ s[2] ^ v[2] ^ v[10];
+        h[3] = h[3] ^ s[3] ^ v[3] ^ v[11];
+        h[4] = h[4] ^ s[0] ^ v[4] ^ v[12];
+        h[5] = h[5] ^ s[1] ^ v[5] ^ v[13];
+        h[6] = h[6] ^ s[2] ^ v[6] ^ v[14];
+        h[7] = h[7] ^ s[3] ^ v[7] ^ v[15];
     }
     private void Initialize (long[] v,long[] h,long[] s,long t0,long t1){
         
